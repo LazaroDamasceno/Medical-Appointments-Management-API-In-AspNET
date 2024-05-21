80a0a7e0c09d3801ac74fbe286e42d8b9b7b64c1
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-05-21 16:33:33 -0300

    Created a temporary controller for getting all medical appointments

---
+++ MedicalAppointments/Find/FindMedicalAppointmentService.cs
@@ -0,0 +1,56 @@
+﻿using MedicalAppointmentsManagementAPI.Doctors;
+using MedicalAppointmentsManagementAPI.Doctors.FindByLicenseNumber;
+using MedicalAppointmentsManagementAPI.Patients;
+using MedicalAppointmentsManagementAPI.Patients.FindBySsn;
+
+namespace MedicalAppointmentsManagementAPI.MedicalAppointments.Find;
+
+public class FindMedicalAppointmentService
+{
+
+    private readonly AppDbContext _context;
+    private readonly FindPatientBySsnService _findPatientBySsn;
+    private readonly FindDoctorByLicenseNumberService _findDoctorByLicenseNumber;
+
+    public FindMedicalAppointmentService(
+        AppDbContext context, 
+        FindPatientBySsnService findPatientBySsn, 
+        FindDoctorByLicenseNumberService findDoctorByLicenseNumber
+    ) {
+        _context = context;
+        _findPatientBySsn = findPatientBySsn;
+        _findDoctorByLicenseNumber = findDoctorByLicenseNumber;
+    }
+
+    public MedicalAppointment Find(string ssn, string doctorLicenseNumber, DateTime scheduledDateTime)
+    {
+        Patient patient = _findPatientBySsn.Find(ssn);
+        Doctor doctor = _findDoctorByLicenseNumber.Find(doctorLicenseNumber);
+        MedicalAppointment? medicalAppointment = _context
+            .MedicalAppointments
+            .First(
+                e => e.Patient == patient
+                && e.Doctor == doctor
+                && e.ScheduledDateTime == scheduledDateTime
+            );
+        if (medicalAppointment == null)
+        {
+            throw new MedicalAppointmentNotFoundException(ssn, doctorLicenseNumber, scheduledDateTime);
+        }
+        return medicalAppointment;
+    }
+
+    public MedicalAppointment Find(string doctorLicenseNumber, DateTime scheduledDateTime)
+    {
+        Doctor doctor = _findDoctorByLicenseNumber.Find(doctorLicenseNumber);
+        MedicalAppointment? medicalAppointment = _context
+            .MedicalAppointments
+            .First(e => e.Doctor == doctor && e.ScheduledDateTime == scheduledDateTime);
+        if (medicalAppointment == null)
+        {
+            throw new MedicalAppointmentNotFoundException(doctorLicenseNumber, scheduledDateTime);
+        }
+        return medicalAppointment;
+    }
+
+}
---
+++ MedicalAppointments/Find/MedicalAppointmentNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace MedicalAppointmentsManagementAPI.MedicalAppointments.Find;
+
+public class MedicalAppointmentNotFoundException : Exception
+{
+    public MedicalAppointmentNotFoundException(string ssn, string doctorLicenseNumber, DateTime scheduledDateTime) : base(
+        $"""
+            Medical appointment whose patient's ssn is {ssn}, doctor's license number is {doctorLicenseNumber} and scheduled date is {scheduledDateTime} time was not found.
+        """)
+    { }
+
+    public MedicalAppointmentNotFoundException(string doctorLicenseNumber, DateTime scheduledDateTime) : base(
+        $"""
+            Medical appointment whose doctor's license number is {doctorLicenseNumber} and scheduled date is {scheduledDateTime} time was not found.
+        """)
+    { }
+}
