92837839440d51342d049483108fdaa2aeb3d1a4
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-05-21 22:35:07 -0300

    Fixed MedicalNoteException

--- MedicalAppointments/AddMedicalNoted/MedicalNoteException.cs
+++ MedicalAppointments/AddMedicalNoted/MedicalNoteException.cs
@@ -4,3 +4,4 @@ public class MedicalNoteException(string doctorLicenseNumber, DateTime dateTime)
     : Exception($"""
                     Medical appointment whose doctor's license number is {doctorLicenseNumber} and scheduled date time is {dateTime} already has medical notes.
                """)
+{  }
