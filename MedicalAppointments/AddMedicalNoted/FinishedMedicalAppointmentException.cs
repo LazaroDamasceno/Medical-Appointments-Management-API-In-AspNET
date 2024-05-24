10edbdc176efab04b78f28c09c08e47411e3f35a
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-05-24 00:03:55 -0300

    Renamed an exception

--- MedicalAppointments/AddMedicalNoted/FinishedMedicalAppointmentException.cs
+++ MedicalAppointments/AddMedicalNoted/FinishedMedicalAppointmentException.cs
@@ -1,6 +1,6 @@
 ﻿namespace MedicalAppointmentsManagementAPI.MedicalAppointments.AddMedicalNoted;
 
-public class MedicalNoteException(string doctorLicenseNumber, DateTime dateTime)
+public class FinishedMedicalAppointmentException(string doctorLicenseNumber, DateTime dateTime)
     : Exception($"""
                     Medical appointment whose doctor's license number is {doctorLicenseNumber} and scheduled date time is {dateTime} already has medical notes.
                """)
