4355a0f9b1e9bd18efee12191ea8e07600b48053
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-05-26 01:19:42 -0300

    Replaced the database

--- Migrations/20240526041626_Initial.Designer.cs
+++ Migrations/20240526041626_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicalAppointmentsManagementAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240526030514_Initial")]
+    [Migration("20240526041626_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
